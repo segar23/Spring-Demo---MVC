1a65e48af5546e4dff9e0d6fe5ff21324c2a6162
Sebastian Garces Palacio <segarces@gmail.com>
2019-05-19 13:15:26 -0400

    Customer Controller Unit Testing
    Unit test for Customer Controller

---
+++ src/test/java/com/mvc/springmvc/controllers/CustomerControllerTest.java
@@ -0,0 +1,166 @@
+package com.mvc.springmvc.controllers;
+
+import com.mvc.springmvc.domain.Customer;
+import com.mvc.springmvc.services.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class CustomerControllerTest {
+
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private CustomerController customerController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void testList() throws Exception{
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer());
+        customers.add(new Customer());
+
+        when(customerService.listAll()).thenReturn((List) customers);
+
+        mockMvc.perform(get("/customer/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customer/list"))
+                .andExpect(model().attribute("customers", hasSize(2)));
+    }
+
+    @Test
+    public void testShow() throws Exception{
+        Integer id = 1;
+
+        when(customerService.getById(id)).thenReturn(new Customer());
+
+        mockMvc.perform(get("/customer/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customer/show"))
+                .andExpect(model().attribute("customer", instanceOf(Customer.class)));
+    }
+
+    @Test
+    public void testEdit() throws Exception{
+        Integer id = 1;
+
+        when(customerService.getById(id)).thenReturn(new Customer());
+
+        mockMvc.perform(get("/customer/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customer/customerform"))
+                .andExpect(model().attribute("customer", instanceOf(Customer.class)));
+    }
+
+    @Test
+    public void testDelete() throws Exception{
+        Integer id = 1;
+
+        mockMvc.perform(get("/customer/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/customer/list"));
+
+        verify(customerService, times(1)).delete(id);
+    }
+
+    @Test
+    public void testNew() throws Exception{
+        Integer id = 1;
+
+        verifyZeroInteractions(customerService);
+
+        mockMvc.perform(get("/customer/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customer/customerform"))
+                .andExpect(model().attribute("customer", instanceOf(Customer.class)));
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception{
+        Integer id = 1;
+        String firstName = "Test Name";
+        String lastName = "Test LName";
+        String email = "test@test.com";
+        String phoneNumber = "666-666-6666";
+        String addressLine1 = "123 Test Dr";
+        String addressLine2 = "";
+        String city = "Test City";
+        String state = "Test State";
+        String zipCode = "66666";
+
+        Customer returnCustomer = new Customer();
+        returnCustomer.setId(id);
+        returnCustomer.setFirstName(firstName);
+        returnCustomer.setLastName(lastName);
+        returnCustomer.setEmail(email);
+        returnCustomer.setPhoneNumber(phoneNumber);
+        returnCustomer.setAddressLine1(addressLine1);
+        returnCustomer.setAddressLine2(addressLine2);
+        returnCustomer.setCity(city);
+        returnCustomer.setState(state);
+        returnCustomer.setZipCode(zipCode);
+
+        when(customerService.saveOrUpdate(ArgumentMatchers.<Customer>any())).thenReturn(returnCustomer);
+
+        mockMvc.perform(post("/customer")
+            .param("id", "1")
+            .param("firstName", firstName)
+            .param("lastName", lastName)
+            .param("email", email)
+            .param("phoneNumber", phoneNumber)
+            .param("addressLine1", addressLine1)
+            .param("addressLine2", addressLine2)
+            .param("city", city)
+            .param("state", state)
+            .param("zipCode", zipCode))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/customer/show/1"))
+                .andExpect(model().attribute("customer", instanceOf(Customer.class)))
+                .andExpect(model().attribute("customer", hasProperty("id", is(id))))
+                .andExpect(model().attribute("customer", hasProperty("firstName", is(firstName))))
+                .andExpect(model().attribute("customer", hasProperty("lastName", is(lastName))))
+                .andExpect(model().attribute("customer", hasProperty("email", is(email))))
+                .andExpect(model().attribute("customer", hasProperty("phoneNumber", is(phoneNumber))))
+                .andExpect(model().attribute("customer", hasProperty("addressLine1", is(addressLine1))))
+                .andExpect(model().attribute("customer", hasProperty("addressLine2", is(addressLine2))))
+                .andExpect(model().attribute("customer", hasProperty("city", is(city))))
+                .andExpect(model().attribute("customer", hasProperty("state", is(state))))
+                .andExpect(model().attribute("customer", hasProperty("zipCode", is(zipCode))));
+
+        ArgumentCaptor<Customer> boundCustomer = ArgumentCaptor.forClass(Customer.class);
+        verify(customerService).saveOrUpdate(boundCustomer.capture());
+
+        assertEquals(id, boundCustomer.getValue().getId());
+        assertEquals(firstName, boundCustomer.getValue().getFirstName());
+        assertEquals(lastName, boundCustomer.getValue().getLastName());
+        assertEquals(email, boundCustomer.getValue().getEmail());
+        assertEquals(phoneNumber, boundCustomer.getValue().getPhoneNumber());
+        assertEquals(addressLine1, boundCustomer.getValue().getAddressLine1());
+        assertEquals(addressLine2, boundCustomer.getValue().getAddressLine2());
+        assertEquals(city, boundCustomer.getValue().getCity());
+        assertEquals(state, boundCustomer.getValue().getState());
+        assertEquals(zipCode, boundCustomer.getValue().getZipCode());
+    }
+}
