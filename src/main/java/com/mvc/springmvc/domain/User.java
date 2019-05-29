cde721549adbcc0488eb08660a8b0530828c732c
Sebastian Garces Palacio <segarces@gmail.com>
2019-05-29 11:29:41 -0400

    Including Cart to Domain
    Including the Cart and the required implementation to the demo

--- src/main/java/com/mvc/springmvc/domain/User.java
+++ src/main/java/com/mvc/springmvc/domain/User.java
@@ -23,6 +23,9 @@ public class User implements DomainObject{
     @OneToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private Customer customer;
 
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
+
 
     @Override
     public Integer getId() {
@@ -81,5 +84,14 @@ public class User implements DomainObject{
     public void setCustomer(Customer customer) {
         this.customer = customer;
         customer.setUser(this);
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+        cart.setUser(this);
     }
 }
--- src/test/java/com/mvc/springmvc/services/UserServiceDAOImplTest.java
+++ src/test/java/com/mvc/springmvc/services/UserServiceDAOImplTest.java
@@ -1,7 +1,6 @@
 package com.mvc.springmvc.services;
 
-import com.mvc.springmvc.domain.Customer;
-import com.mvc.springmvc.domain.User;
+import com.mvc.springmvc.domain.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,17 +9,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @ActiveProfiles("dao")
 public class UserServiceDAOImplTest {
 
     private UserService userService;
+    private ProductService productService;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
 
     @Test
     public void testSaveOrUpdateUser() throws Exception{
@@ -58,5 +66,80 @@ public class UserServiceDAOImplTest {
         assert savedUser.getVersion() != null;
         assert savedUser.getCustomer() != null;
         assert savedUser.getCustomer().getId() != null;
+    }
+
+    @Test
+    public void addCartToUser() throws Exception{
+        User user = new User();
+
+        user.setUsername("segar23");
+        user.setPassword("pass69");
+
+        user.setCart(new Cart());
+
+        User savedUser = userService.saveOrUpdate(user);
+
+        assert savedUser.getId() != null;
+        assert savedUser.getVersion() != null;
+        assert savedUser.getCart() != null;
+        assert savedUser.getCart().getId() != null;
+    }
+
+    @Test
+    public void testAddCartToUserWithCartDetails() throws Exception{
+        User user = new User();
+
+        user.setUsername("segar23");
+        user.setPassword("pass69");
+
+        user.setCart(new Cart());
+
+        List<Product> storedProducts = (List<Product>) productService.listAll();
+
+        CartDetail cartProduct1 = new CartDetail();
+        cartProduct1.setProduct(storedProducts.get(0));
+        user.getCart().addCardDetail(cartProduct1);
+
+        CartDetail cartProduct2 = new CartDetail();
+        cartProduct2.setProduct(storedProducts.get(1));
+        user.getCart().addCardDetail(cartProduct2);
+
+        User savedUser = userService.saveOrUpdate(user);
+
+        assert savedUser.getId() != null;
+        assert savedUser.getVersion() != null;
+        assert savedUser.getCart() != null;
+        assert savedUser.getCart().getId() != null;
+        assert savedUser.getCart().getCartDetails().size() == 2;
+    }
+
+    @Test
+    public void testAddAndRemoveCartToUserWithCartDetails() throws Exception{
+        User user = new User();
+
+        user.setUsername("segar23");
+        user.setPassword("pass69");
+
+        user.setCart(new Cart());
+
+        List<Product> storedProducts = (List<Product>) productService.listAll();
+
+        CartDetail cartProduct1 = new CartDetail();
+        cartProduct1.setProduct(storedProducts.get(0));
+        user.getCart().addCardDetail(cartProduct1);
+
+        CartDetail cartProduct2 = new CartDetail();
+        cartProduct2.setProduct(storedProducts.get(1));
+        user.getCart().addCardDetail(cartProduct2);
+
+        User savedUser = userService.saveOrUpdate(user);
+
+        assert savedUser.getCart().getCartDetails().size() == 2;
+
+        savedUser.getCart().removeCartDetail(savedUser.getCart().getCartDetails().get(0));
+
+        userService.saveOrUpdate(savedUser);
+
+        assert savedUser.getCart().getCartDetails().size() == 1;
     }
 }
